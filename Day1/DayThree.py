a47461c1bd259f6ad521230beebccea10b8c9cf1
Gary Ng <garyng@garys-mbp.attlocal.net>
2024-11-19 18:01:22 -0800

    add more

--- Day1/DayThree.py
+++ Day1/DayThree.py
@@ -6,7 +6,7 @@ def print_todo():
         print(i, item)
 
 while True: 
-    user_action = input("Choose View or Add or Edit or Exit: ")
+    user_action = input("Choose View or Add or Edit or Compelet or  Exit: ")
     match user_action.strip():
         case 'View':
             print_todo()
@@ -18,6 +18,11 @@ while True:
             edit_input = int(input("Which item you want to edit: "))
             new_todo = input("Enter a new item: ")
             todos[edit_input] = new_todo
+            print_todo()
+        case 'Compelet':
+            print_todo()
+            compelet_input = int(input("Which item you are compelet "))
+            todos.pop(compelet_input-1)
             print_todo()
         case 'Exit':
             break
